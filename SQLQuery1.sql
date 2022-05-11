a6c92a1e6c8ac853d6bad15f244f165e74beca7a
Nagasaimanoj8 <nagasaimanoj333@gmail.com>
2022-05-11 11:53:51 +0530

    [Manoj]Adding_DataRetrival

---
+++ SQLQuery1.sql
@@ -0,0 +1,8 @@
+﻿create procedure spGetAllEmployee
+@Name varchar(200),
+@Address varchar(200),
+@Phone bigint,
+@BasicPAy float,
+@Gender char(1)
+AS
+  insert into employeee_payroll(Name,Address,phone,BasicPay,Gender) values(@Name,@Address,@Phone,@BasicPay,@Gender);
